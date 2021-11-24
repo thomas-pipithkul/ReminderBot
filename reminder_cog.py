30ca033342ff8dcff1ff4d3959f15218bdfa08d9
Thomas <suncharn.pipithkul@gmail.com>
2021-11-23 20:20:25 -0500

    Add parse command to debug nlp datetime

--- reminder_cog.py
+++ reminder_cog.py
@@ -208,7 +208,7 @@ class ReminderCog(commands.Cog):
         if ret is not None:
             ret = ret[0]
             parsed_datetime = ret[0]
-            await ctx.send(parsed_datetime.strftime("%m/%d/%Y, %H:%M:%S %Z"))
+            await ctx.send(parsed_datetime.strftime("%m/%d/%Y, %I:%M:%S %Z"))
         else:
             await ctx.send("I don't know")
 
@@ -217,7 +217,7 @@ class ReminderCog(commands.Cog):
         tz = pytz.timezone(self.tz)
         now_utc = pytz.utc.localize(datetime.utcnow())
         now_local = datetime.now().astimezone(tz)
-        await ctx.send(f'now utc: {now_utc.strftime("%m/%d/%Y, %H:%M:%S %Z")}\nnow local: {now_local.strftime("%m/%d/%Y, %H:%M:%S %Z")}')
+        await ctx.send(f'now utc: {now_utc.strftime("%m/%d/%Y, %I:%M:%S %Z")}\nnow local: {now_local.strftime("%m/%d/%Y, %I:%M:%S %Z")}')
 
     @commands.command()
     async def upcoming(self, ctx, *arg):
